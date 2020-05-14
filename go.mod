c04ef4f1330195cc5012f957399910138b834b23
Kelvin Sarink <k.sarink@uni-muenster.de>
2020-05-14 23:16:42 +0200

    made it working under windows

--- go.mod
+++ go.mod
@@ -3,7 +3,19 @@ module photon_explorer_serv
 go 1.14
 
 require (
+	github.com/akavel/rsrc v0.9.0 // indirect
+	github.com/cratonica/2goarray v0.0.0-20190331194516-514510793eaa // indirect
 	github.com/getlantern/systray v0.0.0-20200324212034-d3ab4fd25d99
+	github.com/gobuffalo/envy v1.9.0 // indirect
 	github.com/gobuffalo/packr v1.30.1
+	github.com/gobuffalo/packr/v2 v2.8.0 // indirect
+	github.com/karrick/godirwalk v1.15.6 // indirect
 	github.com/pkg/browser v0.0.0-20180916011732-0a3d74bf9ce4
+	github.com/rogpeppe/go-internal v1.6.0 // indirect
+	github.com/sirupsen/logrus v1.6.0 // indirect
+	github.com/spf13/cobra v1.0.0 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
+	golang.org/x/crypto v0.0.0-20200510223506-06a226fb4e37 // indirect
+	golang.org/x/sync v0.0.0-20200317015054-43a5402ce75a // indirect
+	golang.org/x/sys v0.0.0-20200513112337-417ce2331b5c // indirect
 )
--- main.go
+++ main.go
@@ -13,27 +13,13 @@ import (
 
 func main() {
 	// TODO: check if other instance of photon_explorer_serv is already running and if so just open browser
-	go systray.Run(onReady, onExit)
-
-	box := packr.NewBox("./dist")
-	http.Handle("/", http.FileServer(box))
-	go func() {
-		time.Sleep(100 * time.Millisecond)
-		// TODO: search for free port
-		err := browser.OpenURL("http://localhost:3000")
-		if err != nil {
-			log.Fatal(err)
-		}
-	}()
-	err := http.ListenAndServe("127.0.0.1:3000", nil)
-	if err != nil {
-		log.Fatal(err)
-	}
+	systray.RunWithAppWindow("Photon Explorer", 1024, 768, onReady, onExit)
 }
 
 func onReady() {
-	systray.SetIcon(icon.Photonicon)
+	systray.SetTemplateIcon(icon.Photonicon, icon.Photonicon)
 	systray.SetTooltip("Photon Explorer")
+	systray.SetTitle("Photon Explorer")
 	open := systray.AddMenuItem("Open in browser", "Open Photon Explorer in your browser")
 	quit := systray.AddMenuItem("Quit", "Quit Photon Explorer")
 	go func() {
@@ -50,7 +36,25 @@ func onReady() {
 			log.Fatal(err)
 		}
 	}()
+	startServer()
 }
 
 func onExit() {
+}
+
+func startServer() {
+	box := packr.NewBox("./dist")
+	http.Handle("/", http.FileServer(box))
+	go func() {
+		time.Sleep(100 * time.Millisecond)
+		// TODO: search for free port
+		err := browser.OpenURL("http://localhost:3000")
+		if err != nil {
+			log.Fatal(err)
+		}
+	}()
+	err := http.ListenAndServe("127.0.0.1:3000", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
